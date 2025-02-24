978d6d31e978bfd16d0d72a15f15280292c8f46b
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-24 21:24:12 +0000

    Restyle elements, set width based on viewport

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector("#container");
 const resetButton = document.querySelector("#reset");
+const html = document.querySelector("html");
+
+//console.log(container.firstChild);
 
 function makeGrid(squares) {
     for (let i = 0; i < squares; i++) {
-        let row = document.createElement("p");
+        let row = document.createElement("div");
         for (let j = 0; j < squares; j++) {
             let squareDiv = document.createElement("div");
             squareDiv.className = "squareDiv";
@@ -17,14 +20,34 @@ function makeGrid(squares) {
             });
         }
         row.className = "row";
-        let height = 100 / squares;
-        row.style.height = `${height}vw`;
+        /* let size = 100 / squares;
+        console.log(html.clientHeight);
+        console.log(html.clientWidth); */
+        let windowWidth = html.clientWidth;
+        let windowHeight = html.clientHeight;
+        if (windowWidth <= windowHeight) {
+            let rowWidth = Math.floor(windowWidth * 0.75);
+            row.style.width = `${rowWidth}px`; 
+            row.style.height = `${Math.floor(rowWidth / squares)}px`;
+        } else {
+            let rowWidth = Math.floor(windowHeight * 0.75);
+            row.style.width = `${rowWidth}px`;
+            row.style.height = `${Math.floor(rowWidth / squares)}px`;
+        }
+        /* if (html.clientWidth <= html.clientHeight) {
+            row.style.height = `${size}vw`;
+        } else {
+            row.style.height = `${size}vh`;
+        } */
+        //row.style.height = `${size}vw`;
         container.appendChild(row);
     }
 }
 
+container.addEventListener("load", makeGrid(15));
+
 resetButton.addEventListener("click", () => {
-    while (container.hasChildNodes()) {
+    while (container.hasChildNodes() && container.lastChild.nodeName != "BUTTON") {
         container.removeChild(container.lastChild);
     }
 
